d3d7c9ebd8949d643170adb0c75934f3846bf55b
saidabxl <qunsulka@gmail.com>
2017-12-16 08:22:12 +0100

    Initial commit

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -7,15 +7,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     int goalsTeamA = 0;
     int goalsTeamB = 0;
-    int redCardTeamA = 0;
-    int redCardTeamB = 0;
+    int teamAPenalty = 0;
+    int teamBPenalty = 0;
     int yellowCardTeamA = 0;
     int yellowCardTeamB = 0;
-    int teamAPenalty = 0 ;
-    int teamBPenalty = 0;
+    int redCardTeamA = 0;
+    int redCardTeamB = 0;
+
 
 
     @Override
@@ -24,9 +24,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("teamA", goalsTeamA);
+        savedInstanceState.putInt("teamB", goalsTeamB);
+        savedInstanceState.putInt("PenaltyA", teamAPenalty);
+        savedInstanceState.putInt("PenaltyB", teamBPenalty);
+        savedInstanceState.putInt("YCA", yellowCardTeamA);
+        savedInstanceState.putInt("YCB", yellowCardTeamB);
+        savedInstanceState.putInt("RCA", redCardTeamA);
+        savedInstanceState.putInt("RCB", redCardTeamB);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int goalsTeamA = savedInstanceState.getInt("teamA");
+        int goalsTeamB = savedInstanceState.getInt("teamB");
+        int teamAPenalty = savedInstanceState.getInt("PenaltyA");
+        int teamBPenalty = savedInstanceState.getInt("PenaltyB");
+        int yellowCardTeamA = savedInstanceState.getInt("YCA");
+        int yellowCardTeamB = savedInstanceState.getInt("YCB");
+        int redCardTeamA = savedInstanceState.getInt("RCA");
+        int redCardTeamB = savedInstanceState.getInt("RCB");
+
+        displayForTeamA(0);
+        displayForTeamB(0);
+        yellowForTeamA(0);
+        yellowForTeamB(0);
+        redForTeamA(0);
+        redCardForTeamB(0);
+        teamAPenalty(0);
+        teamBPenalty(0);
+
+    }
 
     /**
-     * Displays the given goals for Team A.
+     * Add Goals for TeamA
+     */
+    public void goalsTeamA(View v) {
+        goalsTeamA = goalsTeamA + 1;
+        displayForTeamA(goalsTeamA);
+
+    }
+
+    /**
+     * Displays the given goals for TeamA.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_goal);
@@ -34,25 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given goals for Team B.
-     */
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_goal);
-        scoreView.setText(String.valueOf(score));
-    }
-
-
-    /**
-     * Add Goals for Team A
-     */
-    public void goalsTeamA(View v) {
-        goalsTeamA = goalsTeamA + 1;
-        displayForTeamA(goalsTeamA);
-    }
-
-
-    /**
-     * Add Goals for Team B
+     * Add Goals for TeamB
      */
     public void goalForTeamB(View v) {
         goalsTeamB = goalsTeamB + 1;
@@ -60,7 +86,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given yellow cards for Team A.
+     * Displays the given goals for TeamB.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_goal);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Add Yellow Card for TeamA
+     */
+    public void yellowCardForTeamA(View v) {
+        yellowCardTeamA = yellowCardTeamA + 1;
+        yellowForTeamA(yellowCardTeamA);
+    }
+
+
+    /**
+     * Displays the given yellow cards for TeamA.
      */
     public void yellowForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_yellow_card);
@@ -69,33 +112,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * Displays the given yellow cards for Team B.
-     */
-    public void yellowForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_yellow_card);
-        scoreView.setText(String.valueOf(score));
-    }
-
-
-    /**
-     * Add Yellow Card for Team A
-     */
-    public void yellowCardForTeamA(View v) {
-        yellowCardTeamA = yellowCardTeamA + 1;
-        yellowForTeamA(yellowCardTeamA);
-    }
-
-    /**
-     * Add Yellow Card for Team B
+     * Add Yellow Card for TeamB
      */
     public void yellowCardForTeamB(View v) {
         yellowCardTeamB = yellowCardTeamB + 1;
         yellowForTeamB(yellowCardTeamB);
     }
 
+    /**
+     * Displays the given yellow cards for TeamB.
+     */
+    public void yellowForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_yellow_card);
+        scoreView.setText(String.valueOf(score));
+    }
 
     /**
-     * Displays the given red cards for Team A.
+     * Add red card for TeamA
+     */
+    public void redCardForTeamA(View v) {
+        redCardTeamA = redCardTeamA + 1;
+        redForTeamA(redCardTeamA);
+    }
+
+    /**
+     * Displays the given red cards for TeamA.
      */
     public void redForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_red_card);
@@ -103,51 +144,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given red cards for Team B.
+     * Add red card for TeamB
      */
-    public void redForTeamB(int score) {
+    public void redCardForTeamB(View v) {
+        redCardTeamB = redCardTeamB + 1;
+        redCardForTeamB(redCardTeamB);
+    }
+
+    /**
+     * Displays the given red cards for TeamB.
+     */
+    public void redCardForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_red_card);
         scoreView.setText(String.valueOf(score));
     }
 
 
     /**
-     *  Add red card for Team A
-     */
-    public void redCardForTeamA(View v) {
-        redCardTeamA = redCardTeamA + 1;
-        redForTeamA(redCardTeamA);
-    }
-
-
-    /**
-     * Add red card for Team B
-     */
-    public void redCardForTeamB(View v) {
-        redCardTeamB = redCardTeamB + 1;
-        redForTeamB(redCardTeamB);
-    }
-
-
-    /**
-     * Add Penalty Card for Team A
+     * Add Penalty Card for TeamA
      */
     public void teamAPenalty(View v) {
         teamAPenalty = teamAPenalty + 1;
         teamAPenalty(teamAPenalty);
     }
 
-    /**
-     * Add Penalty for Team B
-     */
-    public void teamBPenalty(View v) {
-        teamBPenalty = teamBPenalty + 1;
-        teamBPenalty(teamBPenalty);
-    }
-
 
     /**
-     * Displays the Penalty Team A.
+     * Displays the Penalty TeamA.
      */
     public void teamAPenalty(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_penalty);
@@ -155,7 +178,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the Penalty Team A.
+     * Add Penalty for TeamB
+     */
+    public void teamBPenalty(View v) {
+        teamBPenalty = teamBPenalty + 1;
+        teamBPenalty(teamBPenalty);
+    }
+
+    /**
+     * Displays the Penalty TeamB.
      */
     public void teamBPenalty(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_penalty);
@@ -164,26 +195,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method reset all points and all games when the reset all button click.
+     * Reset all score
      */
+
     public void resetScore(View v) {
         goalsTeamA = 0;
         goalsTeamB = 0;
-        redCardTeamA = 0;
-        redCardTeamB = 0;
-        yellowCardTeamA = 0;
-        yellowCardTeamB = 0;
         teamAPenalty = 0;
         teamBPenalty = 0;
+        yellowCardTeamA = 0;
+        yellowCardTeamB = 0;
+        redCardTeamA = 0;
+        redCardTeamB = 0;
 
         displayForTeamA(0);
         displayForTeamB(0);
         yellowForTeamA(0);
         yellowForTeamB(0);
         redForTeamA(0);
-        redForTeamB(0);
+        redCardForTeamB(0);
         teamAPenalty(0);
         teamBPenalty(0);
+
 
     }
 }
